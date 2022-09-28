82bcd41d481efdc573d5a1967dc3666e2625e99c
hamza5645 <97033430+hamza5645@users.noreply.github.com>
2022-09-28 12:04:34 +0200

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Hamza Osama on 28/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
