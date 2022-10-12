9b51b58faaf33dee3966ba0e2a17e8dc2dfcc2ed
hamza5645 <97033430+hamza5645@users.noreply.github.com>
2022-10-12 06:27:20 +0200

    added a list and the basic struct and onDelete

---
+++ iExpense/ExpensItem.swift
@@ -0,0 +1,8 @@
+//
+//  ExpensItem.swift
+//  iExpense
+//
+//  Created by Hamza Osama on 12/10/2022.
+//
+
+import Foundation
---
+++ iExpense/Expenses.swift
@@ -0,0 +1,8 @@
+//
+//  Expenses.swift
+//  iExpense
+//
+//  Created by Hamza Osama on 12/10/2022.
+//
+
+import Foundation
