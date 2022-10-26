7231b6fdd4256d243af6d3e0dccca8dabbfc61c7
hamza5645 <97033430+hamza5645@users.noreply.github.com>
2022-10-26 22:29:12 +0200

    made data being saved when app restarts

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -31,6 +31,13 @@ struct AddView: View {
                     .keyboardType(.decimalPad)
             }
             .navigationTitle("Add New Expense")
+            
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(item)
+                }
+            }
         }
     }
 }
