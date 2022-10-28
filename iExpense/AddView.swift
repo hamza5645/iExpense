539d71fcb29ae8fd1db69532d3181e922231af53
hamza5645 <97033430+hamza5645@users.noreply.github.com>
2022-10-28 11:33:16 +0200

    added final touches

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct AddView: View {
     @ObservedObject var expenses: Expenses
+    @Environment(\.dismiss) var dismiss
     
     @State private var name = ""
     @State private var type = "Personal"
@@ -36,6 +37,7 @@ struct AddView: View {
                 Button("Save") {
                     let item = ExpenseItem(name: name, type: type, amount: amount)
                     expenses.items.append(item)
+                    dismiss()
                 }
             }
         }
--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -15,7 +15,18 @@ struct ContentView: View {
         NavigationView {
             List {
                 ForEach(expenses.items) { item in
-                    Text(item.name)
+                    HStack{
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        
+                        Spacer()
+                        Spacer()
+                        
+                        Text (item.amount, format: .currency (code: "EGP"))
+                    }
                 }
                 .onDelete(perform: removeItems)
             }
